7c6b37dc599187badbf0ff19d997d5899e6b0818
Ashwinkumaresan <achusuchu123@gmail.com>
2025-04-13 23:23:09 +0530

    app'

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from 'react';
+
+function App() {
+  const [deferredPrompt, setDeferredPrompt] = useState(null);
+  const [showInstallBtn, setShowInstallBtn] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener('beforeinstallprompt', (e) => {
+      e.preventDefault();  
+      setDeferredPrompt(e); 
+      setShowInstallBtn(true); 
+    });
+  }, []);
+
+  const handleInstallClick = () => {
+    if (deferredPrompt) {
+      deferredPrompt.prompt();
+      deferredPrompt.userChoice.then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+          console.log('App installed');
+        } else {
+          console.log('App installation rejected');
+        }
+        setDeferredPrompt(null);
+        setShowInstallBtn(false);
+      });
+    }
+  };
+
+  return (
+    <div>
+      <h1>{`Follow -> codebyabi `}</h1>
+      {showInstallBtn && <button onClick={handleInstallClick}>Install App</button>}
+    </div>
+  );
+}
+
+export default App;
